266229ccd6b2177e34e50c7e73ffef2a20d062c1
shepai <dextershepherd@hotmail.co.uk>
2020-11-14 10:55:39 +0000

    Add files via upload

--- shep.py
+++ shep.py
@@ -11,10 +11,11 @@ class MyClient(discord.Client):
         #Assign a bot to each person
         if message.author == self.user:
             return
-        self.bots[message.author]=self.bots.get(message.author,[CB("C:/Users/Dexter Shepherd/Documents/AI/V0.0.9/BOX/testCB/")])
-        cleverBot=self.bots[message.author][0] #get current bot
-        await message.channel.send("@"+message.author.name+" "+cleverBot.chat(message.content))
-        
+        if str(message.channel)=="shep-v009":
+            self.bots[message.author]=self.bots.get(message.author,[CB("C:/Users/Dexter Shepherd/Documents/AI/V0.0.9/BOX/testCB/")])
+            cleverBot=self.bots[message.author][0] #get current bot
+            await message.channel.send("@"+message.author.name+" "+cleverBot.chat(message.content))
+            
 client = MyClient()
-client.run('GZr2Ofa3-B0cJJ1xEz4j3nVerCK0wD0n')
+client.run('T')
     
